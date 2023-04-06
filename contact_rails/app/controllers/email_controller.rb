34c17840cf4ea9d04e9f1514b87937dcc032c1c2
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-06 11:27:30 -0700

    Mixed up name and email in error messages

--- contact_rails/app/controllers/email_controller.rb
+++ contact_rails/app/controllers/email_controller.rb
@@ -9,8 +9,8 @@ class EmailController < ApplicationController
     msg_subject = params[:subject]
     msg_body = params[:body]
 
-    return render json: { message: 'Please include a name with your message.' }, status: :bad_request if sender_email.blank?
-    return render json: { message: 'Please include an email address with your message.' }, status: :bad_request if sender_name.blank?
+    return render json: { message: 'Please include an email address with your message.' }, status: :bad_request if sender_email.blank?
+    return render json: { message: 'Please include a name with your message.' }, status: :bad_request if sender_name.blank?
     return render json: { message: 'Please include a body for your message.' }, status: :bad_request if msg_body.blank?
 
     unless sender_email.match?(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
