39171309c001275a966ce55dfc19a796ce109de6
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-06 12:54:14 -0700

    Simplify css

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -2,28 +2,6 @@ const form = document.getElementById("contactForm");
 const submitButton = form.querySelector("input[type='submit']");
 const messageElement = document.createElement("div");
 
-// add CSS rules for the message element
-const messageStyles = `
-  display: inline;
-  transform: translate(-50%, -50%);
-  color: white;
-  transition: opacity 0.5s ease-in-out;
-  z-index: 10;
-`;
-
-// add CSS rules for the success message
-const successStyles = `
-  background-color: green;
-`;
-
-// add CSS rules for the error message
-const errorStyles = `
-  background-color: red;
-`;
-
-// apply the message styles to the message element
-messageElement.setAttribute("style", messageStyles);
-
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // prevent the default form submission behavior
 
@@ -38,34 +16,16 @@ form.addEventListener("submit", function(event) {
   })
   .then(({ ok, data }) => {
     messageElement.innerText = data.message;
-    messageElement.classList.add("form-message");
 
     // apply the appropriate styles to the message element based on the response status
     if (ok) {
-      messageElement.setAttribute("style", messageStyles + successStyles);
+      messageElement.setAttribute("style", "color: green;");
     } else {
-      messageElement.setAttribute("style", messageStyles + errorStyles);
+      messageElement.setAttribute("style", "color: red;");
     }
 
     // add the message element to the DOM
     submitButton.after(messageElement);
-    messageElement.classList.add("form-message-show");
-
-    // trigger a reflow to apply the new styles before transitioning the opacity
-    messageElement.offsetHeight;
-
-    // fade in the message
-    messageElement.style.opacity = "1";
-
-    if (!ok) {
-      setTimeout(function() {
-        // fade out the message after 5 seconds
-        messageElement.style.opacity = "0";
-        setTimeout(function() {
-          messageElement.remove();
-        }, 500);
-      }, 3000);
-    }
   })
   .catch(error => console.error(error));
 });
