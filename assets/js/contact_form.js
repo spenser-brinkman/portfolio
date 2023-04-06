1346de6802619954aa86d0348aaf5850378c2791
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-06 12:02:10 -0700

    Update to display message instead of create popup

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -1,19 +1,29 @@
 const form = document.getElementById("contactForm");
+const submitButton = form.querySelector("input[type='submit']");
+const messageElement = document.createElement("div");
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
 
-    const formData = new FormData(form);
+  const formData = new FormData(form);
 
-    fetch(form.action, {
-        method: form.method,
-        body: formData
-    })
-    .then(response => response.json()) // parse the HTTP response as JSON
-    .then(data => {
-        // create a pop-up window and display the results
-        const popup = window.open("", "Popup", "width=400,height=400");
-        popup.document.write(JSON.stringify(data));
-    })
-    .catch(error => console.error(error));
+  fetch(form.action, {
+    method: form.method,
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    // create a new message element and insert it after the submit button
+    messageElement.innerText = data.message;
+    messageElement.style.color = response.ok ? "green" : "red";
+    submitButton.after(messageElement);
+
+    // if it's an error message, remove it after 5 seconds
+    if (!response.ok) {
+      setTimeout(function() {
+        messageElement.remove();
+      }, 5000);
+    }
+  })
+  .catch(error => console.error(error));
 });
