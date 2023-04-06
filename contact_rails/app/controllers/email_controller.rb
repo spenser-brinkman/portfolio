71b87b30ca574a31865c128b41078224fd718f3b
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-06 08:30:12 -0700

    Update conroller filename

---
+++ contact_rails/app/controllers/email_controller.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class EmailController < ApplicationController
+  def create
+    payload = {
+      'from' => params[:email],
+      'fromName' => params[:name],
+      'to' => 'bob@example.com',
+      'subject' => params[:subject],
+      'body' => params[:body],
+      'apikey' => Rails.application.credentials.apikey,
+    }
+
+    uri = URI.parse('https://api.elasticemail.com/v2/email/send')
+    request = Net::HTTP::Post.new(uri)
+    request.set_form_data(payload)
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.is_a?(Net::HTTPSuccess)
+      render json: { status: 'success' }, status: :ok
+    else
+      render json: { status: 'failure' }, status: :bad_request
+    end
+  end
+end
