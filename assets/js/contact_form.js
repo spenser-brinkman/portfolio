5329305073e9f093e4a085b902e0b95904b62a7c
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-06 12:21:44 -0700

    Fiddling with message styling

--- assets/js/contact_form.js
+++ assets/js/contact_form.js
@@ -2,8 +2,35 @@ const form = document.getElementById("contactForm");
 const submitButton = form.querySelector("input[type='submit']");
 const messageElement = document.createElement("div");
 
+// add CSS rules for the message element
+const messageStyles = `
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 1em;
+  border-radius: 0.5em;
+  font-size: 1.2em;
+  color: white;
+  transition: opacity 0.5s ease-in-out;
+  z-index: 1;
+`;
+
+// add CSS rules for the success message
+const successStyles = `
+  background-color: green;
+`;
+
+// add CSS rules for the error message
+const errorStyles = `
+  background-color: red;
+`;
+
+// apply the message styles to the message element
+messageElement.setAttribute("style", messageStyles);
+
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
+  event.preventDefault(); // prevent the default form submission behavior
 
   const formData = new FormData(form);
 
@@ -12,24 +39,30 @@ form.addEventListener("submit", function(event) {
     body: formData
   })
   .then(response => {
-    // Capture the response in a variable to pass it along to the next `then` block
     return response.json().then(data => ({ ok: response.ok, data }));
   })
   .then(({ ok, data }) => {
-    // create a new message element and insert it after the submit button
     messageElement.innerText = data.message;
-    messageElement.style.color = ok ? "green" : "red";
+
+    // apply the appropriate styles to the message element based on the response status
+    if (ok) {
+      messageElement.setAttribute("style", messageStyles + successStyles);
+    } else {
+      messageElement.setAttribute("style", messageStyles + errorStyles);
+    }
+
+    // add the message element to the DOM
     submitButton.after(messageElement);
 
-    // add a fade-in effect for both success and error messages
-    messageElement.style.opacity = "0";
-    setTimeout(function() {
-      messageElement.style.opacity = "1";
-    }, 100);
+    // trigger a reflow to apply the new styles before transitioning the opacity
+    messageElement.offsetHeight;
+
+    // fade in the message
+    messageElement.style.opacity = "1";
 
     if (!ok) {
       setTimeout(function() {
-        // add a fade-out effect for error messages only
+        // fade out the message after 5 seconds
         messageElement.style.opacity = "0";
         setTimeout(function() {
           messageElement.remove();
