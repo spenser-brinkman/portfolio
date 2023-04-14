91c20a34ded192dc354b75e83df72cfded9b43df
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-14 12:31:43 -0700

    Refactor for DRYness and HTTParty

--- contact_rails/app/controllers/email_controller.rb
+++ contact_rails/app/controllers/email_controller.rb
@@ -1,50 +1,58 @@
-require 'net/http'
-require 'uri'
-require 'json'
-
 class EmailController < ApplicationController
   def create
     sender_name = params[:name]
-    sender_email = params[:email]
+    sender_address = params[:email]
     msg_subject = params[:subject]
     msg_body = params[:body]
 
-    return render json: { message: 'Please include an email address with your message.' }, status: :bad_request if sender_email.blank?
-    return render json: { message: 'Please include a name with your message.' }, status: :bad_request if sender_name.blank?
-    return render json: { message: 'Please include a body for your message.' }, status: :bad_request if msg_body.blank?
+    return render response(400, 'Please include an email address.') if sender_address.blank?
+    return render response(400, 'Please include a name.') if sender_name.blank?
+    return render response(400, 'Please include a message body.') if msg_body.blank?
 
-    unless sender_email.match?(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
-      return render status: :bad_request,
-                    json: { message: 'The provided email doesn\'t seem to be valid. If you think this error is a mistake, please let me know by emailing me directly at brinkman.spenser@gmail.com.' }
+    unless validate_email(sender_address)
+      return render response(400, "The provided email doesn't seem to be valid. If you think this error is a mistake, please let me know by emailing me directly at brinkman.spenser@gmail.com.")
     end
 
-    payload = {
-      'from' => 'brinkman.spenser@gmail.com',
+    payload = generate_payload(sender_name, sender_address, msg_subject, msg_body)
+
+    res = send_email(payload)
+
+    if res['success']
+      render response(200, 'Message successfully sent!')
+    elsif res['error'].present?
+      render response(400, res['error'])
+    else
+      render response(500, 'There was an error delivering your message. I would appreciate you bringing this to my attention by emailing me directly at brinkman.spenser@gmail.com.')
+    end
+  end
+
+  private
+
+  def validate_email(email)
+    email.match?(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)
+  end
+
+  def response(status, message)
+    {
+      status:,
+      json: { message: }
+    }
+  end
+
+  def generate_payload(sender_name, sender_address, msg_subject, msg_body)
+    {
+      'from' => 'brinkmn.spenser@gmail.com',
       'fromName' => "#{sender_name} (via portfolio)",
-      'replyTo' => sender_email,
+      'replyTo' => sender_address,
       'msgTo' => ['brinkman.spenser@gmail.com'],
       'subject' => msg_subject.present? ? msg_subject : 'A message was sent via your portfolio',
       'body' => "The following message was sent via the contact form on <a href=\"spenserbrinkman.com\">spenserbrinkman.com</a>:<br><br><br>From: #{sender_name}<br>Email: #{sender_email}<br>Subject: #{msg_subject}<br><br>#{msg_body}",
       'apikey' => Rails.application.credentials.apikey
     }
+  end
 
-    uri = URI.parse('https://api.elasticemail.com/v2/email/send')
-    request = Net::HTTP::Post.new(uri)
-    request.set_form_data(payload)
-
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    end
-
-    if (response.body.include? 'success') && (response.body['success'] == false)
-      render status: :bad_request,
-             json: { message: response.body['error'] }
-    elsif response.is_a?(Net::HTTPSuccess)
-      render status: :ok,
-             json: { message: 'Message successfully sent!' }
-    else
-      render status: 500,
-             json: { message: 'There was an error delivering your message. I would appreciate you bringing this to my attention by emailing me directly at brinkman.spenser@gmail.com.' }
-    end
+  def send_email(payload)
+    url = 'https://api.elasticemail.com/v2/email/send'
+    HTTParty.post(url, payload)
   end
 end
