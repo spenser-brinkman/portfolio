e8405afc9e5a8fdc6205808e672ce83a8d8e6663
spenser-brinkman <brinkman.spenser@gmail.com>
2023-04-21 16:00:23 -0700

    Fix misnamed variable

--- contact_rails/app/controllers/email_controller.rb
+++ contact_rails/app/controllers/email_controller.rb
@@ -45,7 +45,7 @@ class EmailController < ApplicationController
       'replyTo' => sender_address,
       'msgTo' => ['brinkman.spenser@gmail.com'],
       'subject' => msg_subject.present? ? msg_subject : 'A message was sent via your portfolio',
-      'body' => "The following message was sent via the contact form on <a href=\"spenserbrinkman.com\">spenserbrinkman.com</a>:<br><br><br>From: #{sender_name}<br>Email: #{sender_email}<br>Subject: #{msg_subject}<br><br>#{msg_body}",
+      'body' => "The following message was sent via the contact form on <a href=\"spenserbrinkman.com\">spenserbrinkman.com</a>:<br><br><br>From: #{sender_name}<br>Email: #{sender_address}<br>Subject: #{msg_subject}<br><br>#{msg_body}",
       'apikey' => Rails.application.credentials.apikey
     }
   end
