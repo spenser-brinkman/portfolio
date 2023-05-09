5156036f2fc5a222d0fcb731695f36d0dff08d6a
spenser-brinkman <brinkman.spenser@gmail.com>
2023-05-09 12:23:21 -0700

    Update spam filter

--- contact_rails/app/helpers/spam_filter.rb
+++ contact_rails/app/helpers/spam_filter.rb
@@ -2,11 +2,12 @@ class SpamFilter
   SPAM_PHRASES = [
     'buy now',
     'best advertising',
-    'earn money fast'
+    'earn money fast',
+    'seo'
   ].freeze
 
   # Returns `true` if filter finds a blacklisted phrase in the provided message
   def self.filter(message)
-    SPAM_PHRASES.any? { |phrase| message.include?(phrase) }
+    SPAM_PHRASES.any? { |phrase| message.downcase.include?(phrase) }
   end
 end
